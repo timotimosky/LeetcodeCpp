8f8d7cc8ceb1fedb21bee66938a7b65e9906566b
timotimosky <timotimosky@users.noreply.github.com>
2019-09-04 01:50:04 +0800

    TEMP

---
+++ LeetCodeCpp/BinaryTreeTemplate.h
@@ -0,0 +1,19 @@
+#pragma once
+
+template<typename ValType>
+class BinaryTnode
+{
+public:
+	ValType value;
+	friend class BinaryTree<ValType>;
+
+};
+
+
+//¶þ²æÊ÷Ä£°å
+class BinaryTreeTemplate
+{
+	//friend class BinaryTree<ValType>;
+
+};
+
